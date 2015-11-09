1d9e99986f040a7f5037bb6835266e93eb3b5c0e
cristhian9403 <cristhian.camilo.alvarez@correounivalle.edu.co>
2015-11-09 17:52:01 -0500

    segunda vista

---
+++ vistas/patron2.php
@@ -0,0 +1,173 @@
+   <!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=divice-width, initial-scale=1.0">
+	<meta charset="utf-8">
+	<title>primer interfaz</title>
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap-responsive.css">
+	<link rel="stylesheet" href="../font-awesome-4.3.0/font-awesome.min.css">
+	<link rel="stylesheet" href="../css/estilo.css">
+
+	<script src="../js/jquery.js"></script>
+	<link rel="stylesheet" type="text/css" href="../js/jquery.js">
+	<script src="../js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="../js/bootstrap.min.js">
+	<script src="../js/java.js"></script>
+	
+
+
+	
+
+
+<body class="fondo">
+
+
+	<div class="container-fluid navbar-inner" >
+	  <div class="row-fluid ">
+	    <div class="span12">
+	     
+	    </div>	    
+	  </div>
+	</div>
+	<div class="container-fluid navbar-inner" >
+	  <div class="row-fluid ">
+	    <div class="span12">
+		    <img src="../imagenes/Untitled.png" alt="">
+		     <form class="navbar-search pull-right">
+			  <div class="form-group">
+			  	<!--<input type="text" class="search-query" placeholder="Search">-->
+			    <input type="text" class="form-control" placeholder="Buscar">
+			  </div>
+			  <br/><button type="submit" class="btn btn-default">Buscar</button>
+			 </form>
+	    </div>	    
+	  </div>
+	</div>
+	<div class="container-fluid navbar-inner">
+	  <div class="row-fluid">
+	    <div class="span12">
+	    	<div class="btn-group">
+			    <a class="btn btn-large"  href="primerainterfaz.html">
+				    Inicio
+				</a>
+			</div>
+			<div class="btn-group">
+			    <a class="btn dropdown-toggle btn-large" data-toggle="dropdown" href="#">
+				    Psicoligía
+				    <span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu">
+				    <li><a href="#">Definicion</a></li>
+				    <li><a href="#">Fundamentos históricos</a></li>
+				    <li><a href="#">Fundamentos biológicos</a></li>
+				    <li><a href="#">Fundamentos filosóficos</a></li>
+				 </ul>
+			</div>
+			<div class="btn-group">
+			    <a class="btn dropdown-toggle btn-large" data-toggle="dropdown" href="#">
+				    Ramas de la Psicoligía
+				    <span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu">
+				    <li><a href="#">Psicoligía Social</a></li>
+				    <li><a href="#">Psicoligía Educativa</a></li>
+				    <li><a href="#">Psicoligía Clinica</a></li>
+				    <li><a href="#">Psicoligía Organizacional</a></li>				 
+				 </ul>
+			</div>
+			<div class="btn-group">
+			    <a class="btn dropdown-toggle btn-large" data-toggle="dropdown" href="#">
+				    Eventos de Psicoligía
+				    <span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu">
+				    <li><a href="#">Action</a></li>
+				    <li><a href="#">Another action</a></li>
+				    <li><a href="#">Something else here</a></li>
+				 </ul>
+			</div>
+			<div class="btn-group">
+			    <a class="btn dropdown-toggle btn-large" data-toggle="dropdown" href="#">
+				    Presentación
+				    <span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu">
+				    <li><a href="#">Action</a></li>
+				    <li><a href="#">Another action</a></li>
+				    <li><a href="#">Something else here</a></li>
+				 </ul>
+			</div>
+	    </div>	    
+	  </div>
+	</div>
+	<div class="container-fluid navbar-inner" >
+		<div class="row-fluid ">
+	    <div class="span2">
+	     	<h1>Psicoligía</h1>
+	    </div>
+	    <div class="span2"></div>
+	    <div class="span8">	
+	    <ul class="breadcrumb">
+			  <li><a href="#">Home</a> <span class="divider">/</span></li>
+			  <li><a href="#">Library</a> <span class="divider">/</span></li>
+			  <li class="active">Data</li>
+			</ul>    
+		</div>
+	  </div>
+	</div>
+	<div class="container-fluid navbar-inner" >
+		<div class="row-fluid ">
+	    <div class="span12">
+	      <div class="tabbable tabs-left">
+		  		<ul class="nav nav-tabs">
+		    		<li class="active">
+		    			<a href="#lA" data-toggle="tab">Section 1</a>
+		    		</li>
+		    		<li class="">
+		    			<a href="#lB" data-toggle="tab">Section 2</a>
+		    		</li>
+		    		<li class="">
+		    			<a href="#lC" data-toggle="tab">Section 3</a>
+		    		</li>
+		  		</ul>
+		  	<div class="tab-content">
+                <div class="tab-pane active" id="lA">
+                  <p>I'm in Section A.</p>
+                </div>
+                <div class="tab-pane" id="lB">
+                  <p>Howdy, I'm in Section B.</p>
+                </div>
+                <div class="tab-pane" id="lC">
+                  <p>What up girl, this is Section C.</p>
+                </div>
+              </div>
+		 </div>
+		</div>
+		<ul class="pager">
+		  <li class="previous">
+		    <a href="#">&larr; Older</a>
+		  </li>
+		  <li class="next">
+		    <a href="#">Newer &rarr;</a>
+		  </li>
+		</ul>	    
+	  </div>
+	</div>
+
+	<div class="container-fluid navbar-inner" >
+	  <div class="row-fluid ">
+	    <div class="span12">
+	     	<img src="../imagenes/Untitled.png" alt="">
+	    </div>	    
+	  </div>
+	</div>
+	
+
+	
+			
+	
+
+</body>
+</head>
+</html>
